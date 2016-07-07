757406c0f77ac22ed3a079a5bdf022faeeba2c8b
LimarenkoDenis <limarenkodenis@gmail.com>
2016-07-07 19:31:41 +0300

    nodemailer

---
+++ postgres/config/gmail.js
@@ -0,0 +1,10 @@
+let transport = {
+    "host": "10.10.53.202",
+    "secureConnection": false,
+    "port": 1025,
+    "ignoreTLS": true,
+    "service": "smtp"
+};
+module.exports = {
+  transport: transport
+};
--- postgres/controller/signUp.js
+++ postgres/controller/signUp.js
@@ -1,11 +1,27 @@
 const path = require('path');
-const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+const config = require(path.join(__dirname, '..', 'config', 'gmail.js'));
 const models = require('../models');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport(config.transport);
 
 module.exports = {
   resources: 'signUp',
   'POST /signUp': (req, res) => {
-    res.send('200')
+    const recipient = req.body.email;
+    const mailOptions = {
+      from: '"Fred Foo 👥" <foo@blurdybloop.com>', // sender address
+      to: recipient, // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'http://localhost:8080/#/email/confirm', // plaintext body
+      html: '<b><a href="http://localhost:8080/#/email/confirm"><a>http://localhost:8080/#/email/confirm</b>' // html body
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+      res.send('200')
+    });
   }
 };
